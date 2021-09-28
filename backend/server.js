64dd09f2f06ce017819bd6afa7fda612262da205
Warit Santaputra <waritsan@gmail.com>
2021-09-28 16:39:21 +0700

    Add mongo

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDatabase = require('./config/database');
 
 // Setting up config file
 dotenv.config({ path: 'config/config.env' });
+
+
+// Connect database
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
