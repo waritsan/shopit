ce8b1d3b371983379bc1f3d271dd21c06004a88a
Warit Santaputra <waritsan@gmail.com>
2021-12-21 23:40:39 +0700

    Store token in cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const sendToken = require('../utils/jwtToken')
 
 // Register user => /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -16,12 +17,7 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
         }
     });
 
-    const token = user.getJwtToken();
-
-    res.status(201).json({
-        success: true,
-        token
-    });
+    sendToken(user, 200, res);
 });
 
 // Login user => /api/v1/login
@@ -47,10 +43,5 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
         return next(new ErrorHandler('Invalid email or password', 401));
     }
 
-    const token = user.getJwtToken();
-
-    res.status(200).json({
-        success: true,
-        token
-    });
+    sendToken(user, 200, res);
 });
