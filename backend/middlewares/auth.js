d597f28c139b90d1b2ba9c2111824f62e64d76f2
Warit Santaputra <waritsan@gmail.com>
2021-12-28 09:43:25 +0700

    Authorize role

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -16,3 +16,13 @@ exports.isAuthenticatedUser = catchAsyncErrors( async (req, res, next) => {
 
     next();
 });
+
+// Handle user roles
+exports.authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(new ErrorHandler(`Role ${req.user.role} is not allowed to access this resource.`, 403));
+        }
+        next();
+    }
+}
