efbcacfda1cdfff9ee7f5f42bf80797d2240976d
Warit Santaputra <waritsan@gmail.com>
2022-01-27 19:12:24 +0700

    Update and delete user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -170,3 +170,66 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
         message: 'Logged out'
     });
 });
+
+// Admin routes
+
+// Get all users => /api/v1/admin/users
+exports.allUsers = catchAsyncErrors (async (req, res, next) => {
+    const users = await User.find();
+
+    res.status(200).json({
+        success: true,
+        users
+    });
+});
+
+// Get user details => /api/v1/admin/user/:id
+exports.getUserDetails = catchAsyncErrors (async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+        return next(new ErrorHandler(`User with id: ${req.params.id} is not found`));
+    }
+
+    res.status(200).json({
+        success: true,
+        user
+    });
+});
+
+// Update user profile => /api/v1/admin/user/:_id
+exports.updateUser = catchAsyncErrors (async (req, res, next) => {
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role
+    }
+
+    const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+        user
+    });
+});
+
+// Delete user => /api/v1/admin/user/:id
+exports.deleteUser = catchAsyncErrors (async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+        return next(new ErrorHandler(`User with id: ${req.params.id} is not found`));
+    }
+
+    // Remove avatar
+
+    await user.remove();
+
+    res.status(200).json({
+        success: true
+    });
+});
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,4 @@
 const express = require('express');
-
-const { isAuthenticatedUser } = require('../middlewares/auth');
-
 const router = express.Router();
 
 const { 
@@ -12,8 +9,14 @@ const {
     getUserProfile,
     updateProfile,
     updatePassword, 
-    logout 
+    logout,
+    allUsers,
+    getUserDetails,
+    updateUser,
+    deleteUser
 } = require('../controllers/authController');
+
+const { isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
@@ -26,5 +29,11 @@ router.route('/logout').get(logout);
 router.route('/me').get(isAuthenticatedUser, getUserProfile);
 router.route('/me/update').put(isAuthenticatedUser, updateProfile);
 router.route('/password/update/').put(isAuthenticatedUser, updatePassword);
+
+router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers);
+router.route('/admin/user/:id')
+    .get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails)
+    .put(isAuthenticatedUser, authorizeRoles('admin'), updateUser)
+    .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteUser);
 
 module.exports = router;
