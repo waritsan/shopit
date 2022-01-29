aadbc55c3b6e8badd997e645726399394f1f64eb
Warit Santaputra <waritsan@gmail.com>
2022-01-29 17:48:08 +0700

    Create new order

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,15 @@ const errorMiddleware = require('./middlewares/errors');
 
 app.use(express.json());
 app.use(cookieParser());
+
 // Import all routes
 const products = require('./routes/product');
 const auth = require('./routes/auth');
+const order = require('./routes/order');
 
 app.use('/api/v1', products);
 app.use('/api/v1/', auth);
+app.use('/api/v1', order);
 
 // Middleware to handle errors
 app.use(errorMiddleware);
