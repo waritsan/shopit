f65c1bf71d658e4ce08408f7593036e0e811658b
Warit Santaputra <waritsan@gmail.com>
2022-01-30 17:41:46 +0700

    Get orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -32,4 +32,28 @@ exports.newOrder = catchAsyncErrors (async (req, res, next) => {
         success: true,
         order
     });
-})
+});
+
+// Get a single order => /api/v1//order/:id
+exports.getSingleOrder = catchAsyncErrors (async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate('user', 'name email');
+
+    if (!order) {
+        return next(new ErrorHandler('No order found with this ID', 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        order
+    });
+});
+
+// Get my orders => /api/v1/orders/me
+exports.myOrders = catchAsyncErrors (async (req, res, next) => {
+    const orders = await Order.find({ user: req.user.id });
+
+    res.status(200).json({
+        success: true,
+        orders
+    });
+});
